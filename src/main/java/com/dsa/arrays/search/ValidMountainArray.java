93db84b7e4ca9a396b89ba0b3f8f26ef5af9210b
ankurpm <ankurpm90@gmail.com>
2022-11-09 18:16:25 -0500

    ValidMountainArray unoptimized solution

--- src/main/java/com/dsa/arrays/search/ValidMountainArray.java
+++ src/main/java/com/dsa/arrays/search/ValidMountainArray.java
@@ -11,4 +11,45 @@ package com.dsa.arrays.search;
  * arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
  */
 public class ValidMountainArray {
+
+    public static void main(String[] args) {
+        //int[] arr = {3,2,1};
+        //int[] arr = {0,1,0};
+        int[] arr = {0,1,3};
+        //int[] arr = {0,1,3,3};
+        //int[] arr = {0,0,1,2,3,2,1};
+        //int[] arr = {0,1,2,4,2,1};
+        //int[] arr = {0,1,2,3,4,5,6,7,8,9};
+
+
+
+        System.out.println(validMountainArray(arr));
+    }
+    public static boolean validMountainArray(int[] arr) {
+        int len = arr.length;
+        if(len<3){
+            return false;
+        }
+        if(arr[0]>=arr[1]){
+            return false;
+        }
+        int i=1;
+        while(i+1<len && arr[i]<arr[i+1]){
+            i++;
+        }
+        if(i==len-1){
+            return false;
+        }
+        if(i+1<len && arr[i] == arr[i+1]){
+            return false;
+        }
+        while(i+1<len && arr[i]>arr[i+1]){
+            i++;
+        }
+        if(i==len-1){
+            return true;
+        }
+        return false;
+
+    }
 }
