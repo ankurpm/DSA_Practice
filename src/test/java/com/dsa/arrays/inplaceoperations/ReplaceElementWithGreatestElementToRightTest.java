64ef775bdee907905956db49b6c0db9e73f4f5e6
ankurpm <ankurpm90@gmail.com>
2022-11-15 20:21:29 -0500

    ReplaceElementWithGreatestElementToRight

---
+++ src/test/java/com/dsa/arrays/inplaceoperations/ReplaceElementWithGreatestElementToRightTest.java
@@ -0,0 +1,32 @@
+package com.dsa.arrays.inplaceoperations;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReplaceElementWithGreatestElementToRightTest {
+
+    ReplaceElementWithGreatestElementToRight ref = new ReplaceElementWithGreatestElementToRight();
+
+    @Test
+    @DisplayName("Test Case 1")
+    void testCase1(){
+        int[] input = {17,18,5,4,6,1};
+        int[] result = ref.replaceWithGreatest(input);
+        int[] output = {18, 6, 6, 6, 1, -1};
+        assertEquals(Arrays.toString(result), Arrays.toString(output));
+    }
+
+    @Test
+    @DisplayName("Test Case 2")
+    void testCase2(){
+        int[] input = {17};
+        int[] result = ref.replaceWithGreatest(input);
+        int[] output = {-1};
+        assertEquals(Arrays.toString(result), Arrays.toString(output));
+    }
+}
