e582a87f59c252bf12728f72c06c3fca1d85f487
ankurpm <ankurpm90@gmail.com>
2022-11-19 19:13:03 -0500

    Sort array by parity (move even numbers to right and odd to left of the array)

---
+++ src/main/java/com/dsa/arrays/inplaceoperations/SortArrayByParity.java
@@ -0,0 +1,20 @@
+package com.dsa.arrays.inplaceoperations;
+
+public class SortArrayByParity {
+
+    public int[] sortArrayByParity(int[] nums) {
+        if (nums.length == 1){
+            return nums;
+        }
+        int left=0;
+        for(int i =0; i< nums.length;i++){
+            if(nums[i] % 2 == 0){
+                int temp = nums[left];
+                nums[left] = nums[i];
+                nums[i] = temp;
+                left++;
+            }
+        }
+        return nums;
+    }
+}
---
+++ src/test/java/com/dsa/arrays/inplaceoperations/SortArrayByParityTest.java
@@ -0,0 +1,42 @@
+package com.dsa.arrays.inplaceoperations;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SortArrayByParityTest {
+
+    SortArrayByParity sbp = new SortArrayByParity();
+
+    @Test
+    @DisplayName("Test case 1")
+    void test1(){
+        int[] arr = {1};
+        int[] result = sbp.sortArrayByParity(arr);
+        assertEquals(Arrays.toString(arr),Arrays.toString(result));
+
+    }
+
+    @Test
+    @DisplayName("Test case 2")
+    void test2(){
+        int[] arr = {3,1,2,4};
+        int[] result = sbp.sortArrayByParity(arr);
+        int[] desired = {2,4,3,1};
+        assertEquals(Arrays.toString(desired),Arrays.toString(result));
+
+    }
+
+    @Test
+    @DisplayName("Test case 3")
+    void test3(){
+        int[] arr = {0,1,2,3,4,5,6};
+        int[] result = sbp.sortArrayByParity(arr);
+        int[] desired = {0, 2, 4, 6, 1, 5, 3};
+        assertEquals(Arrays.toString(desired),Arrays.toString(result));
+
+    }
+}
